740b751bbf1b8634b73e9dd2d92d933c61dcf894
dziubenzo <dziubenzo@gmail.com>
2024-03-11 16:16:58 +0100

    Fix express-rate-limit error in production

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const RateLimit = require('express-rate-limit');
 const limiter = RateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
   max: 20,
+  validate: { xForwardedForHeader: false },
 });
 
 // Set up Mongoose connection
